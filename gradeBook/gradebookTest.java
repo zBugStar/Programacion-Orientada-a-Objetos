360ceb0cef731d49c51d0bea9c5ac4c007c98578
Juan Diego Orozco Meza <161064571+zBugStar@users.noreply.github.com>
2024-04-17 19:15:13 -0500

    Bug fix

--- gradeBook/gradebookTest.java
+++
@@ -1,8 +0,0 @@
-package gradeBook;
-public class gradebookTest {
-    public static void main(String[] args) {
-        gradeBook courseName1 = new gradeBook("numero2");
-        System.out.println(courseName1.displayMessage() + courseName1.getCourseName());
-
-    }
-}
