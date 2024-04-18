059cacfbb8be12b7eebb09589b232b56036004f6
zBugStar <juan.orozco45208@ucaldas.edu.co>
2024-04-18 10:53:04 -0500

    Bug Fix

--- gradeBook/gradeBookTest.java
+++ gradeBook/gradeBookTest.java
@@ -7,20 +7,18 @@ public class gradeBookTest {
         System.out.println(courseName1.displayMessage() + courseName1.getCourseName());
         Scanner input = new Scanner(System.in);
 
-        double total;
-        int gradeCounter;
-        double average;
-        double grade;
-        int pass;
-        int notPass;
+        double total,average,grade;
+        int gradeCounter,notPass,pass,students;
+
+
+        gradeCounter = 1;
+        students = 10;
+        pass = 0;
+        notPass = 0;
 
         total = 0;
-        gradeCounter = 1;
 
-        pass = 0;
-        notPass= 0;
-
-        while(gradeCounter <= 10){
+        while(gradeCounter <= students){
             System.out.println("Escriba la nota estudiante " + gradeCounter);
             grade = input.nextDouble();
             if(grade >=3){
@@ -35,11 +33,12 @@ public class gradeBookTest {
         }
         average = total / 10 ;
 
-        System.out.println("El total de promedio de las notas es : " + average);
+        System.out.println("El promedio de las notas es : " + average);
+
         if(pass >= 8){
-            System.out.println("El profe merece un bono");
+            System.out.println("El profesor merece el bono");
         }else if(notPass >8){
-            System.out.println("No merece el bono");
+            System.out.println("El profesor no merece el bono");
         }
 
     }
