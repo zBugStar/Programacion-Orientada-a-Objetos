d475b035674e179ce345ee990e5ef71b4e8741e2
zBugStar <juan.orozco45208@ucaldas.edu.co>
2024-06-12 15:51:03 -0500

    Agrego interfaz

---
+++ InterfazEncriptado/src/Interfaz/Principal.java
@@ -0,0 +1,23 @@
+package Interfaz;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Principal extends JDialog {
+    private JPanel panel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+
+    public Principal() {
+        setContentPane(panel);
+        setModal(true);
+
+
+
+    }
+
+
+}
